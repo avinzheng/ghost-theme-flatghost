ffadaa5401acc77c7c3d3155a979a30cf2203ef4
Avin Cheng <im@avincheng.com>
2020-07-22 15:43:38 +0800

    styles(widgets): optimize widgets styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 /** ----------------------------------------------------------------------------
  * @author  Avin Cheng
- * @desc    Build and zip theme flat-ghost.
+ * @desc    Build & zip theme flat-ghost.
  * @license MIT
  ** --------------------------------------------------------------------------*/
 const fs = require('fs');
@@ -32,7 +32,7 @@ const cssNano = require('cssnano');
 const DIST_DIR = './dist/';
 const BUILD_DIR = './dist/build/';
 const ASSETS_DIR = './dist/build/assets/';
-const MANIFEST_FILE = path.join('./dist/rev-manifest.json');
+const MANIFEST_FILE = './dist/rev-manifest.json';
 
 /**
  * Clean Files
@@ -80,8 +80,8 @@ function processCss() {
   const urlOptions = {
     url: 'inline',
     encodeType: 'base64',
-    maxSize: 8 * 1024
-  }
+    maxSize: 6 * 1024
+  };
 
   const postCssPlugins = [
     autoPreFixer(),
@@ -122,7 +122,7 @@ function processJs() {
  * Rewrite references to assets revisioned by `gulp-rev`
  */
 function reWriteRefs() {
-  return src(path.join(BUILD_DIR, 'default.hbs'))
+  return src(path.join(BUILD_DIR, './default.hbs'))
     .pipe(revRewrite({ manifest: src(MANIFEST_FILE) }))
     .pipe(dest(BUILD_DIR));
 }
